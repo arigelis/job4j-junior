b3f7f28aab50f283090e14a1c82a9c4f78559e72
arigelis <workstance@mail.ru>
2019-06-18 21:27:41 +0500

    5.1.1. Итератор для двухмерного массива int[][]

---
+++ chapter_001/src/main/java/ru/job4j/JaggerArrayIterator.java
@@ -0,0 +1,43 @@
+package ru.job4j;
+
+import java.util.Iterator;
+
+public class JaggerArrayIterator implements Iterator {
+    int[] value;// = {{1, 2}, {3, 4}};
+    int index = 0;
+
+    public JaggerArrayIterator(int[][] value) {
+        int initiar = 0;
+        for (int i = 0; i < value.length; i++) {
+            for (int j = 0; j < value[i].length; j++) {
+                initiar++;
+            }
+        }
+        int[] tmp = new int[initiar];
+        int counter = 0;
+        for (int i = 0; i < value.length; i++) {
+            for (int j = 0; j < value[i].length; j++) {
+                tmp[counter] = value[i][j];
+                counter++;
+            }
+        }
+        this.value = tmp;
+    }
+
+    public boolean hasNext() {
+        return value.length > index;
+    }
+
+    public Integer next() {
+        if (this.hasNext()) {
+            return value[index++];
+        } else {
+            return 0;
+        }
+    }
+
+    public void remove() {
+
+    }
+}
+
