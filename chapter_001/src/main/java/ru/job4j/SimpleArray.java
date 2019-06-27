f387b6f4491e3de01c4418f1b32b581ca3d3d048
arigelis <workstance@mail.ru>
2019-06-27 22:45:43 +0500

    SimpleArray final

--- chapter_001/src/main/java/ru/job4j/SimpleArray.java
+++ chapter_001/src/main/java/ru/job4j/SimpleArray.java
@@ -5,36 +5,34 @@ import java.util.Iterator;
 
 public class SimpleArray<T> implements Iterable {
     private T[] tmp;
-    int index;
+    int indexArr;
 
     public SimpleArray(int arrayLength) {
         tmp = (T[]) new Object[arrayLength];
     }
 
     public void add(T model) {
-        for (int i = 0; i < tmp.length; i++) {
-            if (tmp[i] == null) {
-                tmp[i] = model;
-                break;
-            }
-        }
+        tmp[indexArr] = model;
+        indexArr++;
     }
 
     public boolean set(int index, T model) {
         boolean result = false;
-        if (index < tmp.length) {
+        if (index < indexArr) {
             tmp[index] = model;
             result = true;
         }
         return result;
     }
 
-    public boolean remove(int index) {//Здесь не знал как именно реализовать удаление, сделал просто =null.
-        boolean result = false;       //Могу переделать со смещением всех последующих элементов, если так правильнее.
-        if (index < tmp.length) {
-            tmp[index] = null;
+    public boolean remove(int index) {
+        boolean result = false;
+        if (index < indexArr) {
+            System.arraycopy(tmp, index, tmp, indexArr, tmp.length - 1);
         }
-        return false;
+
+
+        return result;
     }
 
     public T get(int index) {
