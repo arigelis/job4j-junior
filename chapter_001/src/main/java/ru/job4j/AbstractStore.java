7bba6559c0bfc9a1e40b2833405b5dc86e7347d9
arigelis <workstance@mail.ru>
2019-07-03 20:47:00 +0500

    Abstract class

--- chapter_001/src/main/java/ru/job4j/AbstractStore.java
+++ chapter_001/src/main/java/ru/job4j/AbstractStore.java
@@ -1,6 +1,6 @@
 package ru.job4j;
 
-public abstract class AbstractStore<T extends Base> implements Store<T> {
+public abstract class AbstractStore <T extends Base> implements Store<T>{
     public abstract void add(T model);
 
     public abstract boolean replace(String id, T model);
