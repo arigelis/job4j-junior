d3697278007e552d8cd9630f394d60040eb37c45
arigelis <workstance@mail.ru>
2019-07-03 21:34:33 +0500

    5.3.2. Создать контейнер на базе связанного списка

--- chapter_001/src/main/java/ru/job4j/List/LinkedDynamicArray.java
+++ chapter_001/src/main/java/ru/job4j/List/LinkedDynamicArray.java
@@ -1,41 +1,70 @@
 package ru.job4j.List;
 
-
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 
 public class LinkedDynamicArray<E> implements Iterable<E> {
+    private Node[] container = new Node[10];
+    private int index = 0;
     public int modCount = 0;
-    private DynamicArray<SimpleArrayList<Node>> container;
+    private Node<E> first;
 
+    public int getSize() {
+        return container.length;
+    }
+
+    public void add(E value) {
+        if (index == 0) {
+            first = new Node<>(value);
+            first.previous = null;
+            index++;
+        } else {
+            if (index >= container.length) {
+                grow();//увеличиваем размер контейнера
+            }
+            Node<E> tmp = new Node<>(value);
+            container[index - 1].next = tmp;//Добавляем "следующий" для предыдущего элемента в стэке
+            tmp.previous = container[index - 1];//Добавляем "предыдущий" элемент для текущего в стэке
+            container[index++] = tmp;
+        }
+    }
+
+    public E get(int index) {
+        return (E) container[index];
+    }
 
     private void grow() {
         modCount++;
-//        SimpleArrayList<Node> tmp = new DynamicArray<SimpleArrayList<Node>>();
-//        System.arraycopy(container, 0, tmp, 0, container.getSize());
-//        container = tmp;
+        Node[] tmp = new Node[container.length * 2 + 1];
+        System.arraycopy(container, 0, tmp, 0, container.length);
+        container = tmp;
     }
 
     @Override
     public Iterator<E> iterator() {
         return new Iterator<E>() {
-            int index = 0;
+            int pos = 0;
+            int expectedModCount = modCount;
 
             @Override
             public boolean hasNext() {
-                return index < container.getSize();
+                return pos < container.length;
             }
 
             @Override
             public E next() {
-                return (E) container.get(index++);
+                if (modCount != expectedModCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return (E) container[pos++];
             }
         };
     }
 
     private static class Node<E> {
-
         E data;
         Node<E> next;
+        Node<E> previous;
 
         Node(E data) {
             this.data = data;
