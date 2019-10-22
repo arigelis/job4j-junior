45883d80ec266ee63addc8ce73776be0d7853de4
arigelis <workstance@mail.ru>
2019-10-22 21:45:10 +0500

    SimpleQueue v2 with mods

--- chapter_001/src/main/java/ru/job4j/List/SimpleQueue.java
+++ chapter_001/src/main/java/ru/job4j/List/SimpleQueue.java
@@ -1,7 +1,7 @@
 package ru.job4j.List;
 
 public class SimpleQueue<T> {
-    LinkedDynamicArray list = new LinkedDynamicArray();
+    private LinkedDynamicArray list = new LinkedDynamicArray();
 
     public T poll() {
         return (T) list.deleteFirst();
