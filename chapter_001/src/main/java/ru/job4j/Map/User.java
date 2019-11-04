84ed2db4a3902cbb47f23b53418cb0168528eb09
fedor.teslyuk <fedor.teslyuk@paynet.uz>
2019-11-04 16:02:47 +0500

    Map_Model User v1

---
+++ chapter_001/src/main/java/ru/job4j/Map/User.java
@@ -0,0 +1,42 @@
+package ru.job4j.Map;
+
+import java.util.Calendar;
+
+public class User {
+    private String name;
+    private int children;
+    private Calendar birthady;
+
+    public User(String name, int children, Calendar birthady) {
+        this.name = name;
+        this.children = children;
+        this.birthady = birthady;
+    }
+
+    public User() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public void setChildren(int children) {
+        this.children = children;
+    }
+
+    public Calendar getBirthady() {
+        return birthady;
+    }
+
+    public void setBirthady(Calendar birthady) {
+        this.birthady = birthady;
+    }
+}
