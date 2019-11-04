40591010aaaa07ce8e2d7f825eec124e9b05ebb1
fedor.teslyuk <fedor.teslyuk@paynet.uz>
2019-11-04 14:39:45 +0500

    queue v3

--- chapter_001/src/main/java/ru/job4j/List/SimpleQueue.java
+++ chapter_001/src/main/java/ru/job4j/List/SimpleQueue.java
@@ -1,18 +1,13 @@
 package ru.job4j.List;
 
-import org.testng.annotations.Test;
-
 public class SimpleQueue<T> {
-    private SimpleStack<String> list1 = new SimpleStack<String>();
-    private SimpleStack<String> list2 = new SimpleStack<String>();
+    private LinkedDynamicArray list = new LinkedDynamicArray();
 
-    public SimpleQueue() {
-        list1.push(new String("1"));
-        list1.push(new String("2"));
-        list1.push(new String("3"));
+    public T poll() {
+        return (T) list.deleteFirst();
+    }
 
-        while (!list1.poll().isEmpty()) {
-            list2.push(list1.poll());
-        }
+    public void push(T value) {
+        list.add(value);
     }
 }
