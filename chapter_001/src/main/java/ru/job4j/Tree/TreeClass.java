b20f12911b0b1be9061a54def61da2b021a86dd3
fedor.teslyuk <fedor.teslyuk@paynet.uz>
2019-11-25 15:51:59 +0500

    SimpleTree v3

--- chapter_001/src/main/java/ru/job4j/Tree/TreeClass.java
+++ chapter_001/src/main/java/ru/job4j/Tree/TreeClass.java
@@ -3,23 +3,26 @@ package ru.job4j.Tree;
 import java.util.*;
 import java.util.function.Consumer;
 
-public class TreeClass implements SimpleTree {
+public class TreeClass<E extends Comparable<E>> implements SimpleTree<E> {
     //    private final List<Node> children = new ArrayList<>();
     private Node root = null;
 
     public TreeClass(int i) {
-        root = new Node(i);
+        root = new Node<>(i);
     }
 
     @Override
     public boolean add(E parent, E child) {
+        boolean result = false;
         Optional<Node> tmp = findBy(parent);
+        Node<E> searchingNode = new Node<>(parent);
         if (tmp.isPresent()) {
-            if (!(tmp.get().children.contains(child))) {
+            if (!(tmp.get().leaves().contains(searchingNode))) {
                 tmp.get().add(new Node(child));
+                result = true;
             }
         }
-        return false;
+        return result;
     }
 
 
@@ -43,7 +46,27 @@ public class TreeClass implements SimpleTree {
 
     @Override
     public Iterator iterator() {
-        return null;
+        Queue<Node> data = new LinkedList<>();
+        data.offer(this.root);
+        return new Iterator() {
+            @Override
+            public boolean hasNext() {
+                if (data.isEmpty())
+                    return false;
+                else {
+                    return true;
+                }
+            }
+
+            @Override
+            public Object next() {
+                Node el = data.poll();
+//                for (Object child : el.leaves()) {
+//                    data.offer(((Node) child));
+//                }
+                return el;
+            }
+        };
     }
 
     @Override
