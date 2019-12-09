7fbc6260e27a226721203fad9432b998963b2ad9
fedor.teslyuk <fedor.teslyuk@paynet.uz>
2019-12-09 15:00:26 +0500

    CustomHashMap v3

--- chapter_001/src/main/java/ru/job4j/Map/CustomHashMap.java
+++ chapter_001/src/main/java/ru/job4j/Map/CustomHashMap.java
@@ -1,63 +1,76 @@
 package ru.job4j.Map;
 
-import ru.job4j.Tree.Node;
-
 import java.util.*;
-import java.util.Map.*;
 
 public class CustomHashMap<K, V> {
+    private Node<K, V>[] nodes = new Node[16];
+    int length = 0;
 
     boolean insert(K key, V value) {
+        if (nodes.length - 1 >= length) {
+            nodes[length] = new Node<>(key, value);
+        } else {
+            grow();
+            nodes[length] = new Node<>(key, value);
+        }
         return true;
+    }
+
+    void grow() {
+        Node<K, V>[] tmpNodes = new Node[nodes.length * 3 / 2 + 1];
+        tmpNodes = nodes.clone();
     }
 
     V get(K key) {
-        return null;
+        Iterator it = iterator();
+        V result = null;
+        while (it.hasNext()) {
+            Node tmpNode = ((Node) it.next());
+            if (tmpNode.key.equals(key)) {
+                result = (V) tmpNode.value;
+                break;
+            }
+        }
+        return result;
     }
 
     boolean delete(K key) {
-        return true;
+        Iterator it = iterator();
+        boolean result = false;
+        while (it.hasNext()) {
+            Node tmpNode = ((Node) it.next());
+            if (tmpNode.key.equals(key)) {
+                tmpNode.next = ((Node) it.next()).next;
+                result = true;
+                break;
+            }
+        }
+        return result;
     }
 
     public Iterator iterator() {
         return new Iterator() {
+            int index = 0;
 
             @Override
             public boolean hasNext() {
-                return false;
+                return nodes[index].next != null;
             }
 
             @Override
             public Object next() {
-                return null;
+                return nodes[index++];
             }
         };
     }
 
-    private class Node {
+    private class Node<K, V> {
         K key;
         V value;
-        Node next;
+        Node<K, V> next = null;
 
         public Node(K key, V value) {
             this.key = key;
-            this.value = value;
-        }
-
-
-        public K getKey() {
-            return key;
-        }
-
-        public void setKey(K key) {
-            this.key = key;
-        }
-
-        public V getValue() {
-            return value;
-        }
-
-        public void setValue(V value) {
             this.value = value;
         }
     }
