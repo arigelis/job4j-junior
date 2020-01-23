045986afdbadbd8506647c8e8457f6db3246fbf2
fedor.teslyuk <fedor.teslyuk@paynet.uz>
2020-01-23 22:29:21 +0500

    Analize with hashMap

--- chapter_001/src/main/java/ru/job4j/Analize.java
+++ chapter_001/src/main/java/ru/job4j/Analize.java
@@ -1,30 +1,72 @@
 package ru.job4j;
 
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Analize {
 
 
     public Info diff(List<User> previous, List<User> current) {
         Info result = new Info();
-        int difference = previous.size() - current.size();
-        if (difference > 0) {
-            result.deleted = difference;
-        } else if (difference < 0) {
-            result.added = Math.abs(difference);
-        }
+        //{old realisation
+//        int difference = previous.size() - current.size();
+//        if (difference > 0) {
+//            result.deleted = difference;
+//        } else if (difference < 0) {
+//            result.added = Math.abs(difference);
+//        }
+//
+//        for (int i = 0; i < previous.size(); i++) {
+//            for (int j = 0; j < current.size(); j++) {
+//                if (previous.get(i).id == current.get(j).id) {
+//                    if (!previous.get(i).name.equalsIgnoreCase(current.get(j).name)) {
+//                        result.changed++;
+//                    }
+//                    break;
+//                }
+//            }
+//        }
+//}old
 
-        for (int i = 0; i < previous.size(); i++) {
-            for (int j = 0; j < current.size(); j++) {
-                if (previous.get(i).id == current.get(j).id) {
-                    if (!previous.get(i).name.equalsIgnoreCase(current.get(j).name)) {
-                        result.changed++;
-                    }
-                    break;
-                }
+        Map<Integer, List<String>>
+                previousMap = previous
+                .stream()
+                .collect(
+                        Collectors
+                                .groupingBy(
+                                        User::getId,
+                                        Collectors
+                                                .mapping(
+                                                        User::getName,
+                                                        Collectors
+                                                                .toList())));
+
+        Map<Integer, List<String>>
+                currentMap = current
+                .stream()
+                .collect(
+                        Collectors
+                                .groupingBy(
+                                        User::getId,
+                                        Collectors
+                                                .mapping(
+                                                        User::getName,
+                                                        Collectors
+                                                                .toList())));
+
+//        Iterator iterator = previousMap.entrySet().iterator();
+
+        previousMap.entrySet().forEach(entry -> {
+            if (!currentMap.containsKey(entry.getKey())) {
+                result.deleted++;
+            } else if (!currentMap.get(entry.getKey()).equals(entry.getValue())) {
+                result.changed++;
             }
-        }
-
+        });
+        result.added = current.size() - Math.abs(previous.size() - result.deleted);
         return result;
 
     }
@@ -32,6 +74,22 @@ public class Analize {
     public static class User {
         int id;
         String name;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 
     public static class Info {
