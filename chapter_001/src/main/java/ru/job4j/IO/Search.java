e7475b9a82321155e255470ea34ac98ff23fe874
fedor.teslyuk <fedor.teslyuk@paynet.uz>
2020-03-10 21:35:14 +0500

    Search v2

--- chapter_001/src/main/java/ru/job4j/IO/Search.java
+++ chapter_001/src/main/java/ru/job4j/IO/Search.java
@@ -11,15 +11,21 @@ import java.util.stream.Collectors;
 public class Search {
     List<File> files(String parent, Predicate<String> condition) {
         File file = new File(parent);
+        ArrayList<File> result = new ArrayList<>();
 
         Node<File> fff = new Node<>(file);
         Queue<Node<File>> data = new LinkedList<>();
         data.offer(fff);
-
         while (!data.isEmpty()) {
             Node<File> el = data.poll();
-            data.addAll(el.children);
+            for (File tmpFile : el.getValue().listFiles()) {
+                if (tmpFile.isDirectory() || !condition.test(tmpFile.getName())) {
+                    continue;
+                } else {
+                    result.add(tmpFile);
+                }
+            }
         }
-        return (List<File>) data.stream().filter(e -> !e.getValue().isDirectory()).filter(e -> condition.test(e.getValue().getName()));
+        return result;
     }
 }
