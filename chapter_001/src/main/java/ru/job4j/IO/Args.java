31fd87c607e7f0cf8631b83187e3d6b48037ed24
fedor.teslyuk <fedor.teslyuk@paynet.uz>
2020-03-10 22:07:09 +0500

    Pre-zip builder

---
+++ chapter_001/src/main/java/ru/job4j/IO/Args.java
@@ -0,0 +1,19 @@
+package ru.job4j.IO;
+
+public class Args {
+    public Args() {
+        //String[] args
+    }
+
+    public void directory() {
+
+    }
+
+    public void exclude() {
+
+    }
+
+    public void output() {
+
+    }
+}
