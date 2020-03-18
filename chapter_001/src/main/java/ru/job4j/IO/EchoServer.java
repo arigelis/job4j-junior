3b25d3e55185e98d9e59d6f5184a575e3f679e61
fedor.teslyuk <fedor.teslyuk@paynet.uz>
2020-03-18 21:18:35 +0500

    Bot v3

--- chapter_001/src/main/java/ru/job4j/IO/EchoServer.java
+++ chapter_001/src/main/java/ru/job4j/IO/EchoServer.java
@@ -7,25 +7,21 @@ import java.net.Socket;
 public class EchoServer {
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
-            boolean exit = false;
             while (true) {
-//                if (exit) break;
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
-                    String str, resp = "";
-                    while (!(str = in.readLine()).isEmpty()) {
-                        resp = resp.concat(str);
+                    String str;
+                    if (!(str = in.readLine()).isEmpty()) {
+                        str = str.replace("GET /?msg=", "").replace("HTTP/1.1", "");
                         if (str.contains("exit")) {
-                            exit = true;
-                            str = "finish";
+                            System.out.println("finish");
                             break;
                         }
+                        out.write(("HTTP/1.1 200 " + str + "\r\n").getBytes());
                     }
 
-//                    out.write("HTTP/1.1 200 OK\r\n".getBytes());
-                    out.write(resp.getBytes());
                 }
             }
         }
